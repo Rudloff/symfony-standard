0cbd0708954c8dbdc56ae0058d4ece8df307d90f
Pierre Rudloff <contact@rudloff.pro>
2017-11-12 12:44:23 +0100

    Reproduce issue

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Process\Process;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        //This returns an output
+        dump(shell_exec('dir'));
+
+        $process = new Process('dir');
+        $process->mustRun();
+        //These return empty strings
+        dump(
+            $process->getOutput(),
+            $process->getErrorOutput()
+        );
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
